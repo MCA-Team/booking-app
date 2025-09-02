c96cd82b8f4c6d0d091ac796a9d4f5e016ab52cd
adotri-frimpong <goldmalcom43@gmail.com>
2025-09-02 12:27:51 +0200

    Update of main.go file

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Test Hello world !")
+	var conferenceName string = "Go conference"
+	const conferenceTickets uint = 50 
+	var remainingTickets uint = 50
+	var userName string
+	var userTickets uint
+
+	fmt.Printf("Welcome to %v booking application !\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your ticket here to attend.")
+
+
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
 }
