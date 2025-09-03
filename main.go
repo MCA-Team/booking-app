5664745019db04c6b9e14083cf6506ab0a3c2652
adotri-frimpong <goldmalcom43@gmail.com>
2025-09-03 13:01:38 +0200

    Update of main.go file

--- main.go
+++ main.go
@@ -6,14 +6,33 @@ func main() {
 	var conferenceName string = "Go conference"
 	const conferenceTickets uint = 50 
 	var remainingTickets uint = 50
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets uint
+	var bookings []string
 
+	// Welcome message
 	fmt.Printf("Welcome to %v booking application !\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your ticket here to attend.")
 
+	// Ask user for their informations
+	fmt.Print("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Print("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Print("Enter your e-mail address: ")
+	fmt.Scan(&email)
+	fmt.Print("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
 
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName + " " + lastName)
+
+	// Print the booking confirmation
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation e-mail at %v !\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+	fmt.Printf("Total number of bookers: %v\n", bookings)
 
 }
